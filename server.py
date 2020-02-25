beb9329cc3dd9bf13f418395cf18fc8beed78505
Marius <marius@transloadit.com>
2020-02-25 18:46:26 +0100

    Add first template

--- server.py
+++ server.py
@@ -1,8 +1,8 @@
-from flask import Flask, escape, request
+from flask import Flask, escape, request, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     name = request.args.get("name", "World")
-    return f'Hello, {escape(name)}!'
+    return render_template('index.html', name=name)
