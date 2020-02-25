adeba17fdab6c80bd756f6498501d201a54e7648
Marius <marius@transloadit.com>
2020-02-25 22:20:41 +0100

    Prepare for workshop

--- server.py
+++ server.py
@@ -16,33 +16,7 @@ def index():
 # Assembly has finished
 @app.route('/add_uploads', methods=['POST'])
 def add_uploads():
-	# Get the Assembly from the request body
-	assembly = request.json
-
-	uploads = []
-
-	# Store all thumbnails that were generated
-	for result in assembly['results']['thumbnail']:
-		original_name = result['original_name']
-		original_ext = os.path.splitext(original_name)[1]
-
-		upload = {
-			'original_name': result['original_name'],
-			'file_id':       result['original_id'] + original_ext,
-			'thumbnail_id':  result['id'] + '.' + result['ext']
-		}
-
-		download_file(result['ssl_url'], 'static/thumbnails/' + upload['thumbnail_id'])
-		uploads.append(upload)
-
-	# Store all files which have been uploaded
-	for file in assembly['uploads']:
-		download_file(file['ssl_url'], 'static/files/' + file['id'] + '.' + file['ext'])
-
-	# Save the new entities in the "database"
-	model.add_uploads(uploads)
-
-	return jsonify({'ok': True})
+	# TODO: Add upload saving
 
 # Helper function for downloading a file from an URL
 def download_file(url, destination):
